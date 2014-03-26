9be5a8e685df0a0b26b04347d17be71705771037
mpoiriert <mpoiriert@gmail.com>
2014-03-26 08:30:41 -0400

    bug fix related to class name change

--- Migration/Coordinator.php
+++ Migration/Coordinator.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\DialogHelper;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\OutputInterface;
-use Nucleus\Application\IVariableRegistry;
+use Nucleus\Bundle\CoreBundle\Application\IVariableRegistry;
 
 class Coordinator
 {
